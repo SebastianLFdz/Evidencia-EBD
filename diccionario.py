54042b048c8a683da88054df5c8729c3da1f7c10
OmarGarciaMamuth <126529939+OmarGarciaMamuth@users.noreply.github.com>
2023-03-05 05:49:06 +0000

    update

--- diccionario.py
+++ diccionario.py
@@ -1,6 +1,4 @@
 global diccionario
-diccionario = {1124: ['THE GAME', 'PEPE', 'PEPE', 'ppep', '1234567890123', 'pepe'],
-              1125: ['COTORRO', 'PEPE', 'POPO', 'popo', '789', 'popo'],
-              1126: ['APASITO', 'PEPE', 'APA', 'papa', '789', 'papa']
+diccionario = {
 
 }
