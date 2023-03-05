c4ff8cdb128a44dc35a2952510a4eaf5f2e6fb8d
OmarGarciaMamuth <126529939+OmarGarciaMamuth@users.noreply.github.com>
2023-03-05 05:14:15 +0000

    update

--- diccionario.py
+++ diccionario.py
@@ -1,2 +1,6 @@
 global diccionario
-diccionario = {}
+diccionario = {1124: ['THE GAME', 'PEPE', 'PEPE', 'ppep', '1234567890123', 'pepe'],
+              1125: ['COTORRO', 'PEPE', 'POPO', 'popo', '789', 'popo'],
+              1126: ['APASITO', 'PEPE', 'APA', 'papa', '789', 'papa']
+
+}
