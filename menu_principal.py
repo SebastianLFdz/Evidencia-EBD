3a3290c7b771acfc8ef97ef1b74b84e384e9f2f9
OmarGarciaMamuth <126529939+OmarGarciaMamuth@users.noreply.github.com>
2023-04-23 00:44:24 -0600

    menu_principal.py
    commit

--- menu_principal.py
+++ menu_principal.py
@@ -4,8 +4,7 @@ from diccionario import diccionario
 import csv
 
 def menu():
-    while True:
-        print("""
+    print("""
                      __,                     ,__
                   __/==+\ J. FELIX GARCIA /+==\__
                     "  "`  ===============  '"  "
@@ -29,26 +28,39 @@ def menu():
                 1.- Registrar un nuevo ejemplar
                 2.- Consultar y reportes
                 3.- Salir""")
-        print("\nNOTA: Para indicar que opcion desea realizar introduzca el numero de la opcion deseada")
-        opciones=input("\nIngrese la opcion que desee realizar: ")
-        if opciones =="1":
-            registro()
-        elif opciones=="2":
-            consulta_Y_reporte()
-        elif opciones =="3":
-            descargar = input("Descargar archivo a CSV")
-            if descargar == "S":
-                with open("registros_libreria.csv", "w", newline="") as archivo:
+    print("\nNOTA: Para indicar que opcion desea realizar introduzca el numero de la opcion deseada")
+    while True:
+        try:
+            opciones=int(input("\nIngrese la opcion que desee realizar: "))
+            if opciones ==1:
+                registro()
+            elif opciones==2:
+                consulta_Y_reporte()
+            elif opciones ==3:
+                while True:
+                    print("Introduce una S/N segun lo deseado ")
+                    descargar = input("Descargar archivo a CSV: ").upper()
+                    if descargar == "S":
+                        with open("registros_libreria.csv", "w", newline="") as archivo:
 
-                    grabador = csv.writer(archivo)
-                    grabador.writerow(("clave", "titulo", "autor", "genero", "isbn", "año_publi"))
-                    grabador.writerows((clave, info[0], info[1], info[2], info[3], info[4], info[5]) for clave, info in diccionario.items())  
-            else:
-                print("Gracias por visitar l a biblioteca J. FELIX GARCIA")
+                            grabador = csv.writer(archivo)
+                            grabador.writerow(("clave", "titulo", "autor", "genero", "isbn", "año_publi"))
+                            grabador.writerows((clave, info[0], info[1], info[2], info[3], info[4], info[5]) for clave, info in diccionario.items())  
+                    
+                    elif descargar == "N":
+                        print("Gracias por visitar la biblioteca J. FELIX GARCIA")
+                        break
+                    else:
+                        print("La opcion ingresada no existe")
                 break
-        else:
-            print("\nERROR la opcion elegida no existe.\n\nPor favor indique un numero disponible en el menú")
-            continue
+            else:
+                print("\nERROR la opcion elegida no existe.\n\nPor favor indique un numero disponible en el menú")
+                continue
+        except ValueError: 
+            print("Hay un pequeño error de sintaxis, introduciste ua letra o un simbolo en lugar de un numero")
+        except Exception:
+            print("Error hay un error intentalo de nuevo")
+
         
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 menu()
