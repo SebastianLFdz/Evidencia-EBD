443ee9fa6d121e9d501f01ec84ccd8f1e2a1f5d1
OmarGarciaMamuth <126529939+OmarGarciaMamuth@users.noreply.github.com>
2023-04-23 13:55:22 -0600

    rehistro_libro.py

--- registro_libro.py
+++ registro_libro.py
@@ -1,6 +1,7 @@
 from diccionario import diccionario
 import datetime
 
+
 def registro():
     print("+"+"-"*60+"+")
     fecha_actual = datetime.datetime.now().date()
@@ -10,53 +11,86 @@ def registro():
         lista_datos = []
         while True:
             id_libro=max(diccionario, default=0)+1
-            _titulo=input("Ingresa el titulo del libro que deseas registrar:\n-->")
-            titulo = _titulo.upper()
+            while True:
+                _titulo=input("Ingresa el titulo del libro que deseas registrar:\n-->")
+                if _titulo.isdigit():
+                    print("El dato es un entero")
+                elif _titulo=="":
+                    print("No se admiten valores nulos")
+                else:
+                    titulo = _titulo.upper()
+                    break
             print("+"+"-"*60+"+")
-            _autor=input(f"Ingresa el autor del libro {titulo}:\n-->")
-            autor = _autor.upper()
+
+
+            while True:
+                _autor=input(f"Ingresa el autor del libro {titulo}:\n-->")
+                if _autor.isdigit():
+                    print("El dato es un valor entero")
+                elif _autor=="":
+                    print("No se admiten valores nulos")
+                else:    
+                    autor = _autor.upper()
+                    break
             print("+"+"-"*60+"+")
-            _genero=input(f"Ingresa el genero del libro {titulo}:\n-->")
-            genero = _genero.upper()
+
+            while True:
+                _genero=input(f"Ingresa el genero del libro {titulo}:\n-->")
+                if _genero.isdigit():
+                    print("El  dato es un valor entero")
+                elif _genero =="":
+                    print("No se admiten valores nulos")
+                else:
+                    genero = _genero.upper()
+                    break
+
             print("+"+"-"*60+"+")
             while True:
                 isbn=input(f"Ingresa la clave ISBN del libro {titulo}:\n-->")
                 if len(isbn) == 13:
-                    break
+                    try:
+                        isbn=int(isbn)
+                    except ValueError :
+                        print("Hay un pequeño error de sintaxis, introduciste ua letra o un simbolo en lugar de un numero")
+
+                    else:
+                        break
                 else:
-                    print("¬El ISBN debe tener 13 caracteres, Vuelva a Ingresarlos")
+                    print("El ISBN debe tener 13 caracteres, Vuelva a Ingresarlos")
+                
             while True:
                 print("+"+"-"*60+"+")
-                fecha_publicacion=input(f"Ingresa la fecha de publicacion del libro {titulo} (dd/mmyyyy):\n-->")
-                try:                
+                try:
+                    fecha_publicacion=input(f"Ingresa la fecha de publicacion del libro {titulo} (dd/mm/yyyy):\n-->")
                     fecha_publicacion_procesada = datetime.datetime.strptime(fecha_publicacion, "%d/%m/%Y").date()
-                
+                except ValueError: 
+                    print("Hay un pequeño error de sintaxis, ingresa un dato de tipo fecha con el formato indicado. ")
+                else:
                     publicacion = fecha_publicacion_procesada > fecha_actual
                     if publicacion == True:
                         print(f"La Fecha {fecha_publicacion_procesada}, no es valida, vuelva a ingresarla")
                     else:
                         while True:
                             print("+"+"-"*60+"+")
-                            fecha_adquisicion=input(f"Ingresa la fecha de adquisicion del libro {titulo} (dd/mm/yyyy):\n-->")
-                            fecha_adquisicion_procesada = datetime.datetime.strptime(fecha_adquisicion, "%d/%m/%Y").date()
                             try:
+                                fecha_adquisicion=input(f"Ingresa la fecha de adquisicion del libro {titulo} (dd/mm/yyyy):\n-->")
+                                fecha_adquisicion_procesada = datetime.datetime.strptime(fecha_adquisicion, "%d/%m/%Y").date()
+                            except ValueError: 
+                                print("Hay un pequeño error de sintaxis, ingresa un dato de tipo fecha con el formato indicado. ")
+                            else:
                                 adquisicion = fecha_adquisicion_procesada > fecha_actual
                                 if adquisicion == True:
                                     print(f"La Fecha {fecha_adquisicion_procesada}, no es valida, vuelva a ingresarla")
                                 else:
                                     break
-                            except ValueError:
-                                print(f"El valor proporcionado ({fecha_adquisicion}) no es compatible con la operación solicitada")
-
                         comparacion = fecha_adquisicion_procesada < fecha_publicacion_procesada
                         if comparacion == True:
                             print(f"La Fecha de Adquisicion no puede ser mayor a la Fecha de Publicacion")
                         else:
                             break
-                except ValueError:
-                    print(f"El valor proporcionado ({fecha_publicacion}) no es compatible con la operación solicitada")
             año_publicacion = fecha_publicacion_procesada.year 
             string_adquisicion = str(fecha_adquisicion_procesada)
+
             while True:
                 lista_datos = [titulo,autor,genero, isbn, año_publicacion, string_adquisicion]
                 
@@ -76,41 +110,67 @@ def registro():
                     dato_modificar = int(input("--> "))
                     print("+"+"-"*60+"+")
                     if dato_modificar == 1:
-                        _titulo=input("Ingresa el titulo del libro que deseas registrar:  ")
-                        titulo = _titulo.upper()
-                    elif dato_modificar == 2:
-                        _autor=input(f"Ingresa el nuevo autor del libro {titulo}:  ")
-                        autor= _autor.upper()
-                    elif dato_modificar == 3:
-                        _genero=input(f"Ingresa el nuevo genero del libro {titulo}:  ")
-                        genero = _genero.upper()
-                    elif dato_modificar == 4:
                         while True:
-                            isbn=input(f"Ingresa la nuev clave ISBN {titulo}: ")
-                            if len(isbn) == 13:
-                                break
+                            _titulo=input("Ingresa el titulo del libro que deseas registrar:\n-->")
+                            if _titulo.isdigit():
+                                print("El dato es un entero")
+                            elif _titulo=="":
+                                print("No se admiten valores nulos")
                             else:
-                                print("¬El ISBN debe tener 13 caracteres, Vuelva a Ingresarlos")
+                                titulo = _titulo.upper()
+                                break
+                    elif dato_modificar == 2:
+                        while True:
+                            _autor=input(f"Ingresa el nuevo autor del libro {titulo}:  ")
+                            if _autor.isdigit():
+                                print("El dato es un entero")
+                            elif _titulo=="":
+                                print("No se admiten valores nulos")
+                            else:
+                                autor= _autor.upper()
+                    elif dato_modificar == 3:
+                        while True:
+                            _genero=input(f"Ingresa el nuevo genero del libro {titulo}:  ")
+                            if _genero.isdigit():
+                                print("El dato es un entero")
+                            elif _titulo=="":
+                                print("No se admiten valores nulos")
+                            else:
+                                genero = _genero.upper()
+                    
+                    elif dato_modificar == 4:
+                        isbn=input(f"Ingresa la nuev clave ISBN {titulo}: ")
+                        if len(isbn) == 13:
+                            try:
+                                isbn=int(isbn)
+                            except ValueError :
+                                print("Hay un pequeño error de sintaxis, introduciste ua letra o un simbolo en lugar de un numero")
+                            else:
+                                break
+                        else:
+                            print("El ISBN debe tener 13 caracteres, Vuelva a Ingresarlos")
                     elif dato_modificar == 5:
                         while True:
-                            fecha_publicacion=input(f"Ingresa el nueva fecha de publicacion del libro {titulo} (dd/mm/yyyy): ")
-                            
                             try:
+                                fecha_publicacion=input(f"Ingresa el nueva fecha de publicacion del libro {titulo} (dd/mm/yyyy): ")
                                 fecha_publicacion_procesada = datetime.datetime.strptime(fecha_publicacion, "%d/%m/%Y").date()
+                            except ValueError:
+                                print("Hay un pequeño error de sintaxis, ingresa un dato de tipo fecha con el formato indicado. ")
+                            
+                            else:
                                 if fecha_publicacion_procesada < fecha_actual:
                                     año_publicacion = fecha_publicacion_procesada.year 
                                     break
                                 else:
                                     print(f"La Fecha {fecha_publicacion_procesada}, no es valida, vuelva a ingresarla")
-                
-                            except ValueError:
-                                print(f"La Fecha de Adquisicion no puede ser mayor a la Fecha de Publicacion")
-
                     elif dato_modificar == 6:
                         while True:
-                            fecha_adquisicion=input(f"Ingresa la nueva fecha de adquisicion del libro {titulo}: ")
                             try:
+                                fecha_adquisicion=input(f"Ingresa la nueva fecha de adquisicion del libro {titulo}: ")
                                 fecha_adquisicion_procesada = datetime.datetime.strptime(fecha_adquisicion, "%d/%m/%Y").date()
+                            except ValueError:
+                                print("Hay un pequeño error de sintaxis, ingresa un dato de tipo fecha con el formato indicado. ")
+                            else:
                                 if fecha_adquisicion_procesada < fecha_actual:
                                     if fecha_adquisicion_procesada > fecha_publicacion_procesada:
                                         string_adquisicion = str(fecha_adquisicion_procesada)
@@ -119,13 +179,11 @@ def registro():
                                         print(f"La Fecha de Adquisicion no puede ser mayor a la Fecha de Publicacion")
                                 else:
                                     print(f"La Fecha {fecha_adquisicion_procesada}, no es valida, vuelva a ingresarla")
-                            except ValueError:
-                                print(f"La Fecha de Adquisicion no puede ser mayor a la Fecha de Publicacion")
-
                     else:
                         print("Valor erroneo, Vuelva a Intentarlo")
                 else:
                     print("Respuesta No Aceptada, Vuelva a Ingresarla")
             if validacion.upper() == "S":
                 break 
-    ingresar_datos()
+    #ingresar_datos()
+
